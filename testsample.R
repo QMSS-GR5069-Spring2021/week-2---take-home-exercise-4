30f5c9608e44d6c7beefb06f0d75ecea136e1f86
Xuejing Li <xl2673@columbia.edu>
2021-01-26 12:34:32 -0500

    Add Student Dataframe

--- testsample.R
+++ testsample.R
@@ -1,3 +1,12 @@
 
 
 print("Hello world!") 
+Print("Week2")
+Print("Test)
+
+#Creating New Dataframe
+name <- c('Ace','Simona','Hope')
+id <- c(1,2,3)
+deparment <- c('Math','Literature','Computer Science')
+student.data <- data.frame(name, id, department)
+
